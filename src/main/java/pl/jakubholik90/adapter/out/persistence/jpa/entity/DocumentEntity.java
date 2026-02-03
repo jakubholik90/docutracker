c3110b4d05f9d204bd4874bb5be9d6a67b1ee993
Jakub Holik <jakubholik90@gmail.com>
2026-02-03 23:57:06 +0100

    adding H2 to pom.xml, configuration application.yml

--- src/main/java/pl/jakubholik90/adapter/out/persistence/jpa/entity/DocumentEntity.java
+++ src/main/java/pl/jakubholik90/adapter/out/persistence/jpa/entity/DocumentEntity.java
@@ -1,4 +1,19 @@
 package pl.jakubholik90.adapter.out.persistence.jpa.entity;
 
+import pl.jakubholik90.domain.model.DocumentStatus;
+import pl.jakubholik90.domain.model.RecipientType;
+
+import java.time.LocalDateTime;
+
+// @Entity
 public class DocumentEntity {
+
+    private Integer id;
+    private String fileName;
+    private Integer projectId;
+    private DocumentStatus status;
+    private RecipientType currentRecipient;
+    private LocalDateTime lastStatusChange;
+
+
 }
