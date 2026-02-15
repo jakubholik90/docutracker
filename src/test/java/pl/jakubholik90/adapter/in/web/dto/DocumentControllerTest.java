291869e2705a40ba4c8ef36fa869d64c901c7417
Jakub Holik <jakubholik90@gmail.com>
2026-02-15 23:32:17 +0100

    DocumentControllerTest.java: added test for getDocumentByIdUseCase

--- src/test/java/pl/jakubholik90/adapter/in/web/dto/DocumentControllerTest.java
+++ src/test/java/pl/jakubholik90/adapter/in/web/dto/DocumentControllerTest.java
@@ -6,13 +6,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import pl.jakubholik90.adapter.in.web.DocumentController;
 import pl.jakubholik90.domain.model.Document;
 import pl.jakubholik90.domain.model.DocumentStatus;
-import pl.jakubholik90.domain.port.in.CreateDocumentDTO;
-import pl.jakubholik90.domain.port.in.CreateDocumentUseCase;
+import pl.jakubholik90.domain.port.in.*;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -22,6 +24,9 @@ public class DocumentControllerTest {
 
     @MockitoBean // mock for spring beans
     CreateDocumentUseCase createDocumentUseCase;
+    GetAllDocumentsUseCase getAllDocumentsUseCase;
+    GetDocumentByIdUseCase getDocumentByIdUseCase;
+    GetDocumentsByProjectIdUseCase getDocumentsByProjectIdUseCase;
 
     @Autowired
     MockMvc mockMvc; // MockMvc = simulating HTTP request without starting a server
@@ -78,8 +83,23 @@ public class DocumentControllerTest {
         .andExpect(status().isBadRequest());
 
         verify(createDocumentUseCase,never()).createDocument(any());
+    }
 
+    @Test // testing GetDocumentByIdeUseCase
+    public void shouldReturnDocumentByIdAndStatus200() throws Exception {
+        int id = 1;
+        Document mockDocument = Document.builder()
+                .documentId(id)
+                .build();
 
+        mockMvc.perform(
+                        get("/api/documents/" + id)
+                )
+                // then
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$.id").value(123));
+
+        verify(getDocumentByIdUseCase,times(1)).getDocumentById(id);
     }
 
 
