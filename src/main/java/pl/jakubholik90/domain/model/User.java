c5e9fc3ab934af94dcdf5f28e30f16fac795e3cd
Jakub Holik <jakubholik90@gmail.com>
2026-02-23 17:21:51 +0100

    User.java and UserRepository.java implemented

--- src/main/java/pl/jakubholik90/domain/model/User.java
+++ src/main/java/pl/jakubholik90/domain/model/User.java
@@ -1,4 +1,30 @@
 package pl.jakubholik90.domain.model;
 
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
 public class User {
+
+    private final Long id;
+    private final String email;
+    private final String name;
+    private final LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 }
--- src/main/java/pl/jakubholik90/domain/port/out/UserRepository.java
+++ src/main/java/pl/jakubholik90/domain/port/out/UserRepository.java
@@ -1,4 +1,13 @@
 package pl.jakubholik90.domain.port.out;
 
-public class UserRepository {
+import pl.jakubholik90.domain.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository {
+    public User save(User user);
+
+    public Optional<User> findById(int userId);
+
+    public Optional<User> findByEmail(String email);
 }
